90262d800291f6b667086a99b96e3a2d006973be
jasminSPC <jasmin@stackpointcloud.com>
2016-10-14 01:30:04 +0200

    Version bump

--- profitbricks.js
+++ profitbricks.js
@@ -32,7 +32,7 @@ parseParameters()
 
 function initializeCli() {
     program
-        .version('3.0.1')
+        .version('3.0.2')
         .usage('[Options]')
         .option('setup', 'Configures credentials for ProfitBricks CLI')
         .option('datacenter, [env]', 'Data center operations')
